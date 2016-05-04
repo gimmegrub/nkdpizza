32ee4b567cb7a1f541819e541844601c9bf02ade
Emad Farraj <emadfarraj@emads-mbp-2.attlocal.net>
2016-05-04 08:02:40 -0700

    Commit message

--- www/js/index.js
+++ www/js/index.js
@@ -37,13 +37,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+        var ref = cordova.InAppBrowser.open('http://order.nkdpizza.com', '_blank', 'location=no,toolbar=no');
         console.log('Received Event: ' + id);
     }
 };
